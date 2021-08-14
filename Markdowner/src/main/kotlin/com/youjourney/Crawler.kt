76837cfbecaf4adf10898bc31e81024104268c34
choiyoujun <choiyoujun@outlook.com>
2021-08-14 16:00:23 +0900

    0.1.4
    메인 클래스 변경

--- Markdowner/src/main/kotlin/com/youjourney/Crawler.kt
+++ Markdowner/src/main/kotlin/com/youjourney/Crawler.kt
@@ -8,19 +8,17 @@ fun main(args: Array<String>) {
 //    println("${File("").absolutePath}")
 //    println("${ClassLoader.getSystemClassLoader().getResource(".").getPath()}")
 
-    var fr = FileReader(File("/Users/choiyoujun/Documents/GIT/BOJProblemMarkdonwer/Markdowner/build/libs/configPaths.txt"))
-    var br = BufferedReader(fr)
-
     val edgeOptions = mutableListOf<String>()
-    br.forEachLine { edgeOptions.add(it) }
+    File("/Users/choiyoujun/Documents/GIT/BOJProblemMarkdonwer/Markdowner/build/libs/edgeOptions.txt")
+        .useLines { lines ->
+            lines.forEach { edgeOptions.add(it) }
+        }
 
-    fr = FileReader(File("/Users/choiyoujun/Documents/GIT/BOJProblemMarkdonwer/Markdowner/build/libs/configPaths.txt"))
-    br = BufferedReader(fr)
     val configPaths = mutableListOf<String>()
-    br.forEachLine { configPaths.add(it) }
-
-    br.close()
-    fr.close()
+    File("/Users/choiyoujun/Documents/GIT/BOJProblemMarkdonwer/Markdowner/build/libs/configPaths.txt")
+        .useLines { lines ->
+            lines.forEach { configPaths.add(it) }
+        }
 
     MarkdownMaker(edgeOptions.toTypedArray(), configPaths.toTypedArray(), args[0])
 }
